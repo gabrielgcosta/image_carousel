4f4e24bd0fac39fb33e226a103da169b4394c574
gabrielgcosta <gabriel.costa@codenapp.com>
2024-02-27 21:40:24 -0300

    first commit

---
+++ src/main/java/com/gabrielgcosta/imagecarousel/ImageCarouselApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielgcosta.imagecarousel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCarouselApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageCarouselApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielgcosta/imagecarousel/ImageCarouselApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielgcosta.imagecarousel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCarouselApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
